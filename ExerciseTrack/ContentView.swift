086ca205b25140617cb053abab5f63292c71e8d7
Yunze Li <liyunze0611@163.com>
2020-06-22 08:46:28 +0400

    Initial Commit

---
+++ ExerciseTrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExerciseTrack
+//
+//  Created by Yunze Li on 2020/6/22.
+//  Copyright © 2020 Arctos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
